872e69df5c01e9518c1f098d05b7160972686b7e
ha-matsumu <ha.matsumu@gmail.com>
2018-12-03 15:57:54 +0900

    update main.js

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 //   - コメントとは: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Lexical_grammar#%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%88
 
 // ここに「ブロックスコープ」の説明を記述する
+/* 
+ブロック内で定義された関数・変数には同じブロック内からアクセスすることはできるが、ブロック外からはアクセスできない。
+ちなみにブロックとは「{}」で囲まれた範囲のことをいう。
+*/ 
 
 
 // 課題2: 「コメント」を使って変数の関数スコープの説明をしてください
 //   - コメントとは: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Lexical_grammar#%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%88
 
 // ここに「関数スコープ」の説明を記述する
+/*
+関数内で定義された関数や変数には同じ関数内からはアクセスできるが、関数外からはアクセスできない。
+*/
 
 
 // 課題3: 以下の条件を満たす高階関数を実装してください
@@ -26,6 +33,21 @@
 //           - 第1引数で受けとった `message` の内容を `alert` を使ってアラートダイアログに表示する
 //   - kadai_3関数を実装した直後に「kadai_3(数値, コールバック関数)」を呼び出して、アラートダイアログのメッセージ内容が意図通りであることを確認する
 
+function kadai_3(age, callback) {
+    alert(callback(age));
+}
+
+kadai_3(21, function(message) {
+    if(typeof(message) !== "number"){
+        return "数値が入力されていません";
+    } else if(message >= 20) {
+        return "値は20以上です";
+    } else if(message >= 10) {
+        return "値は10以上20未満です";
+    } else {
+        return "値は10未満です";
+    }
+});
 
 // 課題4: 以下の条件を満たす即時関数を作る
 //   - 2つの引数を受け取る
@@ -33,3 +55,6 @@
 //     - 第2引数: y => 数値
 //   - 処理内容:
 //     - 第1引数のx, 第2引数のyを使って足し算した結果(「x + y」の結果)をconsole.logで出力する。
+(function(x, y) {
+    console.log(x + y);
+})(1, 2)
